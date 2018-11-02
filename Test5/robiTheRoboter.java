e36ffbcd06fd8e52516f07364a6752a70f050204
TheRiPtide <g.zaugg97@gmail.com>
2018-11-02 12:17:17 +0100

    Removed Try-Catch
    Had a problem with a Try-Catch that was unnecessary

--- Test5/robiTheRoboter.java
+++ Test5/robiTheRoboter.java
@@ -1,41 +1,35 @@
 public class robiTheRoboter
 {
-    public static void main(String[] args)
-    {
-        System.out.println(detectIfAngry("dummer roboter"));
-    }
     static boolean detectIfAngry(String input)
     {
-        try
+
+        String[] separateWords;
+        separateWords = input.split("\\s");
+        String alertWord = "roboter";
+
+        String secondWord = separateWords[1].toLowerCase();
+        String thirdWord;
+
+        if (secondWord.contains(alertWord))
         {
-            String[] separateWords;
-            separateWords = input.split("\\s");
-            String alertWord = "roboter";
+            return true;
+        }
+        else if(separateWords.length > 2)
+        {
+            thirdWord = separateWords[2].toLowerCase();
 
-            String secondWord = separateWords[1].toLowerCase();
-            String thirdWord;
-
-            if (secondWord.contains(alertWord))
+            if (thirdWord.contains(alertWord))
             {
                 return true;
             }
-            else if(separateWords.length >= 2)
+            else
             {
-                thirdWord = separateWords[2].toLowerCase();
-
-                if (thirdWord.contains(alertWord))
-                {
-                    return true;
-                }
+                return false;
             }
-            else return false;
         }
-        catch (Exception e)
-        {
-            System.out.println(e);
-            return false;
-        }
-        return false;
+        else return false;
+
+
 
     }
 }
